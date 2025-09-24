382526ec8bfa8d12d05eccd92d4bc3e1a2e4eef1
Rachel Davison <rdavison@smith.edu>
2025-09-24 17:10:24 -0400

    final draft of a3

--- Conversation.java
+++ Conversation.java
@@ -1,32 +1,50 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 class Conversation implements ConversationRequirements {
 
   // Attributes 
-  String transcript = "";
+  private ArrayList<String> transcript;
+  private String[] cannedResponses; 
 
   /**
    * Constructor 
    */
-  Conversation() {
-    this.transcript = "";
+  public Conversation() {
+    this.transcript = new ArrayList<String>();
+    this.transcript.add("\nTRANSCRIPT:");
+    this.cannedResponses = new String[] {
+      "Interesting, tell me more.",
+      "Hmmm.",
+      "Do you really think so?",
+      "You don't say.",
+      "Oh, I see.",
+      "That's cool.",
+      "Fascinating.",
+      "Wow!",
+      "Could you elaborate on that?",
+      "I understand.",
+    };
   }
 
   /**
-   * Starts and runs the conversation with the user
+   * Starts and runs the conversation with the user, and adds it to the transcript
    */
   public void chat() {
     Scanner input = new Scanner(System.in);
     System.out.println("How many rounds?");
     int rounds = input.nextInt();
+    input.nextLine(); // consume the newline character
     System.out.println("Hi there! What's on your mind?");
-    this.transcript += "Hi there! What's on your mind?\n";
+    this.transcript.add("Hi there! What's on your mind?");
     for (int i = 0; i < rounds; i++) {
       String userInput = input.nextLine();
-      this.transcript += userInput + "\n";
+      this.transcript.add(userInput);
       String response = respond(userInput);
       System.out.println(response);
-      this.transcript += response + "\n";
+      this.transcript.add(response);
     }
+    System.out.println("See ya!");
+    this.transcript.add("See ya!");
     input.close();
   }
 
@@ -34,92 +52,196 @@ class Conversation implements ConversationRequirements {
    * Prints transcript of conversation
    */
   public void printTranscript() {
-    System.out.println(this.transcript);
+    for (int i = 0; i < this.transcript.size(); i++){
+      System.out.println(this.transcript.get(i));
+    }
   }
 
   /**
-   * Gives appropriate response (mirrored or canned) to user input
-   * @param inputString the users last line of input
-   * @return mirrored or canned response to user input  
+   * Checks if the substring is a word in the input string
+   * @param inputString the entire input string 
+   * @param i the starting index of the substring
+   * @param j the ending index of the substring
+   * @return true if the substring is a whole word based on spaces and punctuation, false otherwise
    */
-  public String respond(String inputString) {
-    String response = "";
-    boolean mirror = false;
-    if (inputString.contains("I ")){
-      response = inputString.replace("I ", "You ");
-      mirror = true;
-    }
-    if (inputString.contains(" me ")){
-      response = inputString.replace(" me ", " you ");
-      mirror = true;
-    }
-    if (inputString.contains(" my ")){
-      response = inputString.replace(" my ", " your ");
-      mirror = true;
-    }
-    if (inputString.contains(" am ")){
-      response = inputString.replace(" am ", " are ");
-      mirror = true;
-    }
-    if (inputString.contains("Me ")){
-      response = inputString.replace("Me ", "You ");
-      mirror = true;
-    }
-    if (inputString.contains("My ")){
-      response = inputString.replace("My ", "Your ");
-      mirror = true;
-    }
-    if (inputString.contains(" you ")){
-      response = inputString.replace(" you ", " I ");
-      mirror = true;
-    }
-    if (inputString.contains(" me,")){
-      response = inputString.replace(" me, ", " you,");
-      mirror = true;
-    }
-    if (inputString.contains("You ")){
-      response = inputString.replace("You ", "I ");
-      mirror = true;
-    }
-    if (inputString.contains(" your ")){
-      response = inputString.replace(" your ", " my ");
-      mirror = true;
-    }
-    if (inputString.contains("Your ")){
-      response = inputString.replace("Your ", "My ");
-      mirror = true;
-    }
-    if (mirror){
-      if (response.contains(".")){
-        response = response.replace(".", "?");
-      }
-      if (response.contains("!")){
-        response = response.replace("!", "?");
-      }
-      if (!response.contains("?")){
-        response += "?";
+  private boolean isAWord(String inputString, int i, int j){
+    if (i == 0 || inputString.substring(i-1, i).equals(" ") || inputString.substring(i-2, i).equals(". ") || inputString.substring(i-2, i).equals(", ") || inputString.substring(i-2, i).equals("! ") || inputString.substring(i-2, i).equals("? ")){
+      if (j == inputString.length() || inputString.substring(j, j+1).equals(" ") || inputString.substring(j, j+1).equals(".") || inputString.substring(j, j+1).equals(",") || inputString.substring(j, j+1).equals("!") || inputString.substring(j, j+1).equals("?")){
+        return true;
       }
     }
-    else{
-      String[] cannedResponses = {
-        "Interesting, tell me more.",
-        "Hmmm.",
-        "Do you really think so?",
-        "You don't say.",
-        "Oh, I see.",
-        "That's cool."
-      };
-      int randomIndex = (int)(Math.random() * 6);
-      response = cannedResponses[randomIndex];
-    }
-    return response;
+    return false;
   }
 
-  public static void main(String[] arguments) {
+  /** 
+   * Checks if the substring is at the beginning of a sentence in the input string
+   * @param inputString the entire input string 
+   * @param i the starting index of the substring
+   * @param j the ending index of the substring
+   * @return true if the substring is at the beginning of a sentence, false otherwise
+   */
+  private boolean afterPunctuation(String inputString, int i, int j){
+    return (inputString.substring(i-2, i).equals(". ") || inputString.substring(i-2, i).equals("! ") || inputString.substring(i-2, i).equals("? "));
+  }
+
+  /**
+   * Gives appropriate response (mirrored or canned) to a string
+   * @param inputString the string being responded to
+   * @return mirrored or canned response to the input string  
+   */
+  public String respond(String inputString) {
+    boolean mirror = false;
+    for (int i = 0; i < inputString.length(); i++){
+      for (int j = i; j <= inputString.length(); j++){
+        // Check for keywords I and me, change them to you
+        if ((inputString.substring(i, j).equalsIgnoreCase("I") || inputString.substring (i, j).equalsIgnoreCase("me")) && isAWord(inputString, i, j)){
+          // if keyword is at the beginning of the entire string, capitalize the response
+          if (i == 0 || (i == 1 && inputString.substring(0,1).equals(" "))){
+            inputString = "You" + inputString.substring(j);
+          }
+          // if keyword is at the beginning of a sentence, capitalize the response
+          else if (afterPunctuation(inputString, i, j)){
+            inputString = inputString.substring(0,i) + "You" + inputString.substring(j);
+          }
+          // if it's not at the beginning of a sentence, don't capitalize the response
+          else{
+            inputString = inputString.substring(0,i) + "you" + inputString.substring(j);
+          }
+          mirror = true; // this is used later to determine if a question mark should be added, as they are only added to mirrored responses
+          i = i + 3; // this skips past the mirrorered word so it isn't remirrored back to its original state
+          j = i; //this makes sure j is always ahead of i
+        }
+
+        // Check for keyword am, change to are
+        else if (inputString.substring(i,j).equalsIgnoreCase("am") && isAWord(inputString, i, j)){
+          if (i == 0 || (i == 1 && inputString.substring(0,1).equals(" "))){
+            inputString = "Are" + inputString.substring(j);
+          }
+          else if (afterPunctuation(inputString, i, j)){
+            inputString = inputString.substring(0,i) + "Are" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "are" + inputString.substring(j);
+          }
+          mirror = true; 
+          i = i + 3;
+          j = i;
+        }
+        // Check for keyword my, change to your
+        else if (inputString.substring(i, j).equalsIgnoreCase("my") && isAWord(inputString, i, j)){
+          if (i == 0 || (i == 1 && inputString.substring(0,1).equals(" "))){
+            inputString = "Your" + inputString.substring(j);
+          }
+          else if (afterPunctuation(inputString, i, j)){
+            inputString = inputString.substring(0,i) + "Your" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "your" + inputString.substring(j);
+          }
+          mirror = true;
+          i = i + 4;
+          j = i;
+        }
+        // Check for keyword you, change to I
+        else if (inputString.substring(i, j).equalsIgnoreCase("you") && isAWord(inputString, i, j)){
+          if (i == 0){
+            inputString = "I" + inputString.substring(j);
+          }
+          // Check if "you" is preceded by "to ", "at ", "towards ", "with ", "for ", "without ", change to "me"
+          else if ((i > 3 && inputString.substring(i-3, i).equals("at ")) || (i > 4 && inputString.substring(i-4, i).equals("for ")) || (i > 3 && inputString.substring(i-3, i).equalsIgnoreCase("to ")) || (i > 8 && inputString.substring(i-8, i).equalsIgnoreCase("towards ")) || (i > 5 && inputString.substring(i-5, i).equalsIgnoreCase("with ")) || (i > 8 && inputString.substring(i-8, i).equalsIgnoreCase("without "))){
+            inputString = inputString.substring(0,i) + "me" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "I" + inputString.substring(j);
+          }
+          mirror = true;
+          i = i + 1;
+          j = i;
+        }
+        // Check for keyword are, change to am
+        else if (inputString.substring(i, j).equalsIgnoreCase("are") && isAWord(inputString, i, j)){
+          if (i == 0 || (i == 1 && inputString.substring(0,1).equals(" "))){
+            inputString = "Am" + inputString.substring(j);
+          }
+          else if (afterPunctuation(inputString, i, j)){
+            inputString = inputString.substring(0,i) + "Am" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "am" + inputString.substring(j);
+          }
+          mirror = true;
+          i = i + 2;
+          j = i;
+        }
+        // Check for keyword your, change to my
+        else if (inputString.substring(i, j).equalsIgnoreCase("your") && isAWord(inputString, i, j)){
+          if (i == 0 || (i == 1 && inputString.substring(0,1).equals(" "))){
+            inputString = "My" + inputString.substring(j);
+          }
+          else if (afterPunctuation(inputString, i, j)){
+            inputString = inputString.substring(0,i) + "My" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "my" + inputString.substring(j);
+          }
+          mirror = true;
+          i = i + 2;
+          j = i;
+        }
+        // Check for keyword I'm, change to You're
+        else if (inputString.substring(i,j).equalsIgnoreCase("I'm") && isAWord(inputString, i, j)){
+          if (i == 0 || (i == 1 && inputString.substring(0,1).equals(" "))){
+            inputString = "You're" + inputString.substring(j);
+          }
+          else if (afterPunctuation(inputString, i, j)){
+            inputString = inputString.substring(0,i) + "You're" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "you're" + inputString.substring(j);
+          }
+          mirror = true;
+          i = i + 6;
+          j = i;
+        }
+        // Check for keyword you're, change to I'm
+        else if (inputString.substring(i,j).equalsIgnoreCase("you're") && isAWord(inputString, i, j)){
+          if (i == 0){
+            inputString = "I'm" + inputString.substring(j);
+          }
+          else{
+            inputString = inputString.substring(0,i) + "I'm" + inputString.substring(j);
+          }
+          mirror = true;
+          i = i + 3;
+          j = i;
+        }      
+      }
+    }
+
+    // If a mirroring was done, ensure the response ends in a question mark, and any sentences within it end in a question mark
+    if (mirror){
+      if (inputString.contains(".")){
+        inputString = inputString.replace(".", "?");
+      }
+      if (inputString.contains("!")){
+        inputString = inputString.replace("!", "?");
+      }
+      if (!inputString.contains("?")){
+        inputString += "?";
+      }
+    }
+    // If no mirroring was done, give a random canned response
+    else{
+      int randomIndex = (int)(Math.random() * this.cannedResponses.length);
+      inputString = this.cannedResponses[randomIndex];
+    }
+    return inputString;
+  }
+
+  public static void main(String[] arguments){
 
     Conversation myConversation = new Conversation();
     myConversation.chat();
     myConversation.printTranscript();
-
   }
 }
--- reflection.md
+++ reflection.md
@@ -6,4 +6,16 @@ Use this file to record your reflection on this assignment.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
 
-split string into words
+Before working on mirroring, I wrote the basic code that asks for the number of rounds and then loops for that number, and wrote a bit of code that adds a question mark to a statement if that statement has been mirrored.
+
+My first idea was to split the String into words, but I ran into a lot of problems because I didn't know how to use the Dictionary tool. Then I decided to simplify using the contains and replace methods, but I didn't trust that I'd think of all the permutations of mirror words and punctuation. Finally, I decided to iterate through the String using a nested for loop, and create a different method to check if the bit of String I was looking at was a word. Then I made else-if-statements to determine if the word was a word I could mirror and if so, mirror it. This created the problem that the next time I iterated through the String it undid the mirror by mirroring it back to how it was in the first place. To fix this, I added at the end of each else-if an i + (the length of the mirror word) so that it started the next iteration past the word it just changed, and a i + j to keep the j number equal to or past the i number. Finally, when I looked at the rubric I realised the transcript was supposed to be an array rather than a tring, so I rewrote that bit of the code.
+
+I worked with Eliana on this assignment. We met up rather late in our coding, so our solutions don't look much alike, but it was nice to see the Dictionary solution done well, since that was one of my first ideas that I abandoned, and her method of implementing it used a lot less repeated code than my iteration did. We tried to also work with Elle, but she didn't show up when we met up.
+
+I went to the session of office hours at Monday 7:00 PM with Binah.
+
+This wasn't my initial impression of Java, because I took AP Computer Science A in high school, but my impression of Java during this assignment was that it does a lot more things for you than I thought it did. I learned Java without a lot of the methods I'm now discovering, so my problem-solving tends to be very skewed towards iteration through things rather than using methods to do that kind of thing for me. 
+
+I don't know any languages other than Java, so I don't know what possible differences might say about programming in general, but I'd guess that the existence of differences between programming languages shows that programming is something that can be done from many different perspectives and ways of solving problems, and that the variety in languages reflects that variety of perspectives.
+
+To someone taking this course in the future, I might advise them to not give up on a way of doing something so quickly. I solved the problem, and my solution does work, but looking at Eliana's code made me wish that I'd tried the new methods I was unfamiliar with, because solving this problem with a new area of Java would have taught me new things about Java, while solving it the way I'm familiar with, while challenging and interesting, didn't really teach me new things about Java.
